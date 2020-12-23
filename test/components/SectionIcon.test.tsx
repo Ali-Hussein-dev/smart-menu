21ea703e38528770c71aad6c62f87af323513752
Ali <ali.hussein.pre@gmail.com>
2020-12-23 18:56:54 +0100

    refactor(SectionIcon): update props, add missing svg
    reduce checks number by dividing icons into two groups based on menu

--- test/components/SectionIcon.test.tsx
+++ test/components/SectionIcon.test.tsx
@@ -4,17 +4,25 @@ import SectionIcon from '../../src/components/SectionIcon'
 
 describe('Section Icon', () => {
   it('burger icon matches snapshot', () => {
-    const { container } = render(<SectionIcon iconName="burger" />)
+    const { container } = render(
+      <SectionIcon menu="dishes" iconName="burger" />
+    )
     expect(container).toMatchSnapshot()
   })
   it('vegan icon matches snapshot with customColor', () => {
     const { container } = render(
-      <SectionIcon iconName="vegan" customColor="bg-orange-500" />
+      <SectionIcon menu="dishes" iconName="vegan" customColor="bg-orange-500" />
     )
     expect(container).toMatchSnapshot()
   })
-  it('vegan icon matches snapshot with default color', () => {
-    const { container } = render(<SectionIcon iconName="vegan" />)
+  it('vegan icon matches dnapshot with default color', () => {
+    const { container } = render(<SectionIcon menu="dishes" iconName="vegan" />)
+    expect(container).toMatchSnapshot()
+  })
+  it('softdrinks icon matches snapshot with default color', () => {
+    const { container } = render(
+      <SectionIcon menu="drinks" iconName="softdrinks" />
+    )
     expect(container).toMatchSnapshot()
   })
 })
