65cee8592d70d3578e05811bb05f1810ce6008e5
Ali <ali.hussein.pre@gmail.com>
2021-01-27 09:53:08 +0100

    refactor,feat,fix(price)
    refactor: removed unnecessary tags rendering, aria-label as string not as obj
    
    feat: add size&units to price
    
    fix: line through only items with hasHH when happy hour is true

--- src/components/leaf-node/Price.tsx
+++ src/components/leaf-node/Price.tsx
@@ -1,61 +1,105 @@
 import { ImInfo } from 'react-icons/im'
-interface Types {
+import * as React from 'react'
+import { Ctx } from '../../context'
+import { Divider } from '@chakra-ui/react'
+
+const ToFixed: React.FC<{
+  num: number | undefined
+  unit?: Menu.SizeUnit
+  size?: number
+  isHH?: boolean
+}> = ({ num, unit, size, isHH = false }) => (
+  <span className="flex justify-center gap-x-1">
+    {typeof num === 'number' && isHH ? (
+      <span className="text-sm line-through text-blueGray-400">
+        {num.toFixed(2)}
+      </span>
+    ) : (
+      <span>{typeof num === 'number' && num.toFixed(2)}</span>
+    )}
+    {(size || unit) && (
+      <span className="text-blueGray-400">
+        {size}
+        {size ? unit : null}
+      </span>
+    )}
+  </span>
+)
+interface PriceProps {
   price: Menu.Price
+  size?: Menu.Size
   isOpen?: boolean
   isInfo?: boolean
+  hasHH?: boolean
   sizeUnit?: Menu.SizeUnit
   onToggle?: (isOpen: boolean) => void
 }
-const ToFixed: React.FC<{
-  num: number | undefined
-  unit?: string
-  size?: number
-  lineThrough?: boolean
-}> = ({ num, unit, size, lineThrough }) => (
-  <span
-    className={`${
-      lineThrough ? 'line-through text-xsm text-blueGray-400' : ''
-    } text-blueGray-800`}
-  >
-    {typeof num === 'number' && num.toFixed(2)}
-    <span className="text-blueGray-500">
-      {' '}
-      {size}
-      {unit}
-    </span>
-  </span>
-)
 //=======================
-export const Price: React.FC<Types> = ({
+export const Price: React.FC<PriceProps> = ({
   price,
+  size,
   onToggle,
   isOpen,
   isInfo = false,
-  // sizeUnit,
+  sizeUnit,
+  hasHH,
 }) => {
   // hooks
+  const { state } = React.useContext(Ctx)
+  const { meta } = state
   //--------------------------------------
   // functions
   //--------------------------------------
   return (
-    <div className="flex flex-col items-center">
-      {' '}
-      {/* need to be  */}
-      <div className="flex flex-col">
-        <ToFixed num={price.p1} />
-        <ToFixed num={price.p2} />
+    <div className="flex flex-col items-center justify-between ml-1 text-blueGray-600">
+      <div className="flex items-center justify-center">
+        {hasHH && meta.isHH && (
+          <span className="flex flex-col mr-1">
+            {price.pp1 && <ToFixed num={price.pp1} />}
+            {price.pp2 && <ToFixed num={price.pp2} />}
+          </span>
+        )}
+
+        {hasHH ? ( // ----hh price
+          <span className="flex flex-col">
+            {price.p1 && (
+              <ToFixed
+                isHH={meta.isHH}
+                num={price.p1}
+                size={size?.s1}
+                unit={sizeUnit}
+              />
+            )}
+            {price.p2 && (
+              <ToFixed isHH={meta.isHH} num={price.p2} unit={sizeUnit} />
+            )}
+          </span>
+        ) : (
+          <span className="flex flex-col">
+            {price.p1 && (
+              <ToFixed num={price.p1} size={size?.s1} unit={sizeUnit} />
+            )}
+            {price.p2 && (
+              <ToFixed num={price.p2} size={size?.s2} unit={sizeUnit} />
+            )}
+          </span>
+        )}
       </div>
-      <button
-        aria-label={`info-${price}`}
-        id={`info-${price}`}
-        onClick={() => {
-          onToggle(!isOpen)
-        }}
-        role="button"
-        className="p-1"
-      >
-        {isInfo && <ImInfo size="15" />}
-      </button>
+      <div className="flex flex-col items-center w-full">
+        {isInfo && (
+          <button
+            aria-label="item-info"
+            onClick={() => {
+              onToggle(!isOpen)
+            }}
+            role="button"
+            className="pb-1"
+          >
+            <ImInfo size="15" className="text-blue-300 " />
+          </button>
+        )}
+        <Divider variant="dashed" />
+      </div>
     </div>
   )
 }
--- test/components/price.test.tsx
+++ test/components/price.test.tsx
@@ -1,19 +1,50 @@
-import { render } from '@testing-library/react'
+import { render } from '../testUtils'
+import { Ctx } from '../../src/context'
 import { Price } from '../../src/components/leaf-node'
-const price = {
-  p1: 4.5,
-  p2: 2.5,
-}
+import MegaList from '../../database/index'
+import * as React from 'react'
+
 /**
- *  missing cases
- * - snapshot: with info button, it requires hook call
- * - snapshot: prices in Happy Hour
- * - snapshot: prices with size unit
- * - user Events: info btn clicked
+ * ?note:
+ * do not expect Drawer to be in the dom because it is rendered by DisheItem, price is just send an event
+ * 
  */
+
 describe('Price', () => {
-  test('price matches snapshot', () => {
-    const { container } = render(<Price price={price} />)
+  let price
+  let size
+  beforeEach(() => {
+    price = {
+      p1: 9.5,
+      p2: 12.5,
+      pp1: 4.4,
+      pp2: 8.4,
+    }
+    size = { s1: 0.3, s2: 0.5 }
+  })
+
+  test('Is price visible', () => {
+    const { container } = render(
+      <Ctx.Provider value={{ state: MegaList[0] }}>
+        <Price price={price} />
+      </Ctx.Provider>
+    )
+    expect(container).toMatchSnapshot()
+  })
+  test('Is info button visible', () => {
+    const { container } = render(
+      <Ctx.Provider value={{ state: MegaList[0] }}>
+        <Price price={price} isInfo={true} />
+      </Ctx.Provider>
+    )
+    expect(container).toMatchSnapshot()
+  })
+  test('Is price visible with size & units', () => {
+    const { container } = render(
+      <Ctx.Provider value={{ state: MegaList[0] }}>
+        <Price price={price} size={size} sizeUnit="L" />
+      </Ctx.Provider>
+    )
     expect(container).toMatchSnapshot()
   })
 })
