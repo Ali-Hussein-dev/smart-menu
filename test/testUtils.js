e12aac426a565942d3396e7f4cc0fe869bd61bfd
Ali <ali.hussein.pre@gmail.com>
2021-01-31 16:22:09 +0100

    test(config) add provider

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { render } from '@testing-library/react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Ctx } from 'src/context'
+import MegaList from '../database'
+import * as React from 'react'
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
-
 const Providers = ({ children }) => {
   // return children
   return (
     <ChakraProvider>
       {/* <TranslationProvider messages={defaultStrings}> */}
-      {children}
+      <Ctx.Provider value={{ state: MegaList[0] }}>{children}</Ctx.Provider>
       {/* </TranslationProvider> */}
     </ChakraProvider>
   )
