c787d97f6ae89c96655b6fc011591cda6e998b6b
Ali <ali.hussein.pre@gmail.com>
2021-02-02 16:19:05 +0100

    feat(UI: swiper integaration) add swiper to menu page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import '../css/global.css'
 import Head from 'next/head'
+import 'swiper/swiper.min.css'
+import 'swiper/components/pagination/pagination.min.css'
+import 'swiper/components/effect-coverflow/effect-coverflow.min.css'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function MyApp({ Component, pageProps }: AppProps) {
