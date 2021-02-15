64dd9de232c22627da93dbb6a6ed922351a094fe
Ali <ali.hussein.pre@gmail.com>
2021-02-15 22:49:52 +0100

    style (declaration) used wrong syntax

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,3 +1,2 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import * as types1 from './types1.d.ts'
-import * as types2 from './types2.d.ts'
+/// <reference types="./types1.d.ts" />
+/// <reference types="./types2.d.ts" />
