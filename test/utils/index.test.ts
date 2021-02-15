9ce9ce8d6ea25c712d947e672620de286c140b57
Ali <ali.hussein.pre@gmail.com>
2021-02-15 14:29:44 +0100

    test (improvement)
    add use cases, update use cases names

--- test/utils/index.test.ts
+++ test/utils/index.test.ts
@@ -1,22 +1,38 @@
-import { convertNumToTimeFormat, withinTimeRange } from '../../src/utils/index'
-
-describe('withTimeRange', () => {
+import {
+  convertNumToTimeFormat,
+  withinTimeRange,
+  validateTime,
+} from '../../src/utils/index'
+//--------------------------------------withinTimeRange
+describe('withinTimeRange', () => {
   let currentTime, startTime, endTime, finalValue
-  test('expect false', () => {
+  test('expect false WHEN currentTime is out of time range', () => {
     currentTime = 1000
     startTime = 1020
     endTime = 1140
     finalValue = withinTimeRange(currentTime, startTime, endTime)
     expect(finalValue).toBe(false)
+    currentTime = 1141
+    finalValue = withinTimeRange(currentTime, startTime, endTime)
+    expect(finalValue).toBe(false)
   })
-  test('expect true', () => {
+  test('expect true WHEN currentTime is within time range', () => {
     currentTime = 1020
     startTime = 1020
     endTime = 1140
+
+    finalValue = withinTimeRange(currentTime, startTime, endTime)
+    expect(finalValue).toBe(true)
+
+    currentTime = 1021
+    finalValue = withinTimeRange(currentTime, startTime, endTime)
+    expect(finalValue).toBe(true)
+
+    currentTime = 1140
     finalValue = withinTimeRange(currentTime, startTime, endTime)
     expect(finalValue).toBe(true)
   })
-  test('expect true WHEN startTime & endTime are not in the day', () => {
+  test('expect true WHEN currentTime is within time range but belongs to the next day', () => {
     currentTime = 20
     startTime = 1350
     endTime = 30
@@ -24,8 +40,8 @@ describe('withTimeRange', () => {
     expect(finalValue).toBe(true)
   })
 })
-
-describe('formatTime', () => {
+//--------------------------------------convertNumToTimeFormat
+describe('convertNumToTimeFormat', () => {
   let timeFormat
   test('Expect 3min to be formated to 03', () => {
     timeFormat = convertNumToTimeFormat(3, 'mm')
@@ -43,4 +59,31 @@ describe('formatTime', () => {
     timeFormat = convertNumToTimeFormat(13, 'HH')
     expect(timeFormat).toBe('13')
   })
+  test('should throw an error WHEN passing invalid number out of 0-60', () => {
+    expect(() => {
+      convertNumToTimeFormat(61, 'mm')
+    }).toThrowError(/first Param must have value between 0-60/i)
+
+    expect(() => {
+      convertNumToTimeFormat(25, 'HH')
+    }).toThrowError(/first Param must have value between 0-24/i)
+
+    expect(() => {
+      convertNumToTimeFormat(-1, 'HH')
+    }).toThrowError(/first Param must have value between 0-24/i)
+  })
 })
+//--------------------------------------validateTime
+describe('validateTime', () => {
+  test('should throw an error WHEN passing number > 1440', () => {
+    expect(() => {
+      validateTime(1441)
+    }).toThrow()
+  })
+  test('should throw an error WHEN passing number < 0', () => {
+    expect(() => {
+      validateTime(-1)
+    }).toThrow()
+  })
+})
+//--------------------------------------getIntervals
